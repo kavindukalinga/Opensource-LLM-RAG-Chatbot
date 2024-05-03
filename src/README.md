427d24ed551f298accb2e3f9843e8edd14aba27b
kavindukalinga <kavindukalingayu@gmail.com>
2024-05-03 18:14:40 +0530

    Add SourceCode

---
+++ src/README.md
@@ -0,0 +1,17 @@
+# Chat-bot App
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+/bin/python3 "/home/kkalinga/Documents/ISA intern/rag-search/Opensource-LLM-RAG-Chatbot/src/populate_database.py"
+# Number of existing documents in DB: 65
+# ✅ No new documents to add
+```
+
+```bash
+/bin/python3 "/home/kkalinga/Documents/ISA intern/rag-search/Opensource-LLM-RAG-Chatbot/src/query_data.py" "What is Grafana?"
+# Response: Based on the provided context, I'd say that Grafana is a popular open-source platform for building beautiful and interactive dashboards to visualize and explore data from multiple sources.
+# Sources: []
+```
---
+++ src/get_embedding_function.py
@@ -0,0 +1,60 @@
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings.huggingface import HuggingFaceInferenceAPIEmbeddings
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+
+
+# model = "Alibaba-NLP/gte-large-en-v1.5"
+# model = "sentence-transformers/all-MiniLM-l6-v2"
+
+# def get_embedding_function():
+#     embeddings = HuggingFaceEmbeddings(model_name=model)
+#     return embeddings
+
+#----------------------------------------------------------------------------------------------------------------------
+# def get_embedding_function():
+#     embeddings = HuggingFaceEmbeddings(model_name="Salesforce/SFR-Embedding-Mistral")
+#     return embeddings
+
+#----------------------------------------------------------------------------------------------------------------------
+# def get_embedding_function():
+#     embeddings=OllamaEmbeddings(model="nomic-embed-text")
+#     return embeddings
+
+
+#----------------------------------------------------------------------------------------------------------------------
+def get_embedding_function():
+    embeddings=OllamaEmbeddings(model="mxbai-embed-large:latest")
+    return embeddings
+
+#----------------------------------------------------------------------------------------------------------------------
+# import dotenv
+# import os
+
+# dotenv.load_dotenv()
+# inference_api_key=os.getenv("huggingface_api_key")
+
+# def get_embedding_function():
+#     embeddings = HuggingFaceInferenceAPIEmbeddings(
+#         api_key=inference_api_key, model_name="sentence-transformers/all-MiniLM-l6-v2"
+#     )
+#     return embeddings
+
+#----------------------------------------------------------------------------------------------------------------------
+# def get_embedding_function():
+#     embeddings = BedrockEmbedd
+# inference_api_key='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'ings(
+#         credentials_profile_name="default", region_name="us-east-1"
+#     )
+#     # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+#     return embeddings
+
+
+#----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    embeddings=get_embedding_function()
+    text = "This is a test document."
+    query_result = embeddings.embed_query(text)
+    print(query_result[:3])
+    
